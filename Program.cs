4f0c7ad870e2dce8261941e4b68a166ee1dc2308
Maria Luiza <malualper@hotmail.com>
2020-07-05 21:29:10 -0300

    fibonacci

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              Console.Write("Digite um número: ");
+            int numero=Convert.ToInt32(Console.ReadLine());
+
+            int fi1=0;
+            int fi2=1;
+            int soma=fi1 + fi2;
+
+            Console.Write(0 + " " + 1 +" ");
+            
+            for (int i = 3; i <=numero; i++)
+            {
+                Console.Write(soma + " ");
+                fi1=fi2;
+                fi2=soma;
+                soma=fi1 + fi2;
+            }
+
+        }
+    }
+}
+    
+
